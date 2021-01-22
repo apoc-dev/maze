2eb071c86efb299f2cf87fc4a90608def8e22542
apocalyps0 <37179739+apocalyps0@users.noreply.github.com>
2021-01-22 14:32:43 +0100

    implement backtrack

--- src/App.java
+++ src/App.java
@@ -1,10 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        int[][] board = {{0,0,0,1},
-                        {1,1,0,1},
-                        {1,1,0,1},
-                        {1,0,0,0}};
+        int[][] board = {{0,0,0,1,1},
+                         {1,1,0,1,1},
+                         {1,0,0,1,1},
+                         {1,0,1,1,1},
+                         {1,0,0,0,0}};
 
         Maze m = new Maze(board);
         if(m.solve()){
--- src/Maze.java
+++ src/Maze.java
@@ -4,10 +4,11 @@ public class Maze {
     int[][] sol;
 
     public Maze(int[][] board){
-        this.sol = new int[4][4];
+        this.sol = new int[board.length][board.length];
+        this.sol[0][0] = 1;
         this.board = board;
     }
-    
+
     public boolean solve(){
         if(backtrack(0,0, sol)){
             return true;
@@ -15,35 +16,51 @@ public class Maze {
         return false;
     }
 
-
-    //backtrack
-    //base case
-    //next step (create id for each field 0-15 rows*col)
-    //if correct execute self
-    //if wrong write field to null and counter -1 and execute self
-
     public boolean backtrack(int row, int col, int[][] sol){
 
         if(row == board.length-1 && col == board.length-1){
             return true;
         }
         
-        
-        for (int i = 0; i < 1; i++) {
+        for (int i = 0; i < 4; i++) {
             if(i == 0){
                 if(check(row, col+1)){
                     sol[row][col+1] = 1;
-                    if(!backtrack(row, col+1, sol)){
-                        sol[row][col+1] = 0;
+                    printboard();
+                    if(backtrack(row, col+1, sol)){
+                        return true;
                     }
+                    sol[row][col+1] = 0;
                 }
             }
             if(i == 1){
                 if(check(row+1, col)){
                     sol[row+1][col] = 1;
-                    if(!backtrack(row+1, col, sol)){
-                        sol[row+1][col] = 0;
+                    printboard();
+                    if(backtrack(row+1, col, sol)){
+                        return true;
                     }
+                    sol[row+1][col] = 0;
+                }
+            }
+            if(i == 2){
+                if(check(row, col-1)){
+                    sol[row][col-1] = 1;
+                    printboard();
+                    if(backtrack(row, col-1, sol)){
+                        return true;
+                    }
+                    sol[row][col-1] = 0;
+                }
+            }
+            if(i == 3){
+                if(check(row-1, col)){
+                    sol[row-1][col] = 1;
+                    printboard();
+                    if(backtrack(row-1, col, sol)){
+                        return true;
+                    }
+                    sol[row-1][col] = 0;
                 }
             }
         }
@@ -51,38 +68,48 @@ public class Maze {
         return false;
     }
 
-
     public boolean check(int row, int col){
-        if(row >= board.length-1){
-            return false;
-        }
-        if(col >= board.length-1){
+        if(row < 0){
             return false;
         }
 
-        if (board[row][col] == 0){
+        if(col < 0){
+            return false;
+        }
+
+        if(row >= board.length){
+            return false;
+        }
+
+        if(col >= board.length){
+            return false;
+        }
+
+        if (board[row][col] == 0 && sol[row][col] != 1){
             return true;
         }
 
         return false;
     }
-    //checkifok
-    //if field is 1 ok
-    //if field is 0 not ok
 
     public void printboard(){
+        System.out.println("-----------");
         System.out.println();
         for (int i = 0; i < board.length; i++) {
+            System.out.print("|");
             for (int j = 0; j < board.length; j++) {
                 System.out.print(board[i][j]);
+                System.out.print("|");
             }
             System.out.println();
             
         }
         System.out.println();
         for (int i = 0; i < board.length; i++) {
+            System.out.print("|");
             for (int j = 0; j < board.length; j++) {
                 System.out.print(sol[i][j]);
+                System.out.print("|");
             }
             System.out.println();
         }
